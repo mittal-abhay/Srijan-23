8f70cf3d640d43ba98eadbd086f23f18c8810124
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-01-01 17:38:50 +0530

    k

---
+++ build/precache-manifest.8c04edd786dd76f95c221c6cb59954bf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af8e1f097cb423d5cd11e22b55d9944",
+    "url": "/Srijan-23/index.html"
+  },
+  {
+    "revision": "092cea73c73669b6984f",
+    "url": "/Srijan-23/static/css/2.7ea48c83.chunk.css"
+  },
+  {
+    "revision": "6e68e6f4d4c53e549d58",
+    "url": "/Srijan-23/static/css/main.12fce728.chunk.css"
+  },
+  {
+    "revision": "092cea73c73669b6984f",
+    "url": "/Srijan-23/static/js/2.5b053d59.chunk.js"
+  },
+  {
+    "revision": "6e68e6f4d4c53e549d58",
+    "url": "/Srijan-23/static/js/main.b386fb40.chunk.js"
+  },
+  {
+    "revision": "1240834f292b5d62331f",
+    "url": "/Srijan-23/static/js/runtime-main.1707868c.js"
+  },
+  {
+    "revision": "25e27511d4c8b94f0716b8438adac02a",
+    "url": "/Srijan-23/static/media/bg-wall.25e27511.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/srijan/precache-manifest.3ba12a018eaa1c8c6763b60bc19ef745.js"
+  "/Srijan-23/precache-manifest.8c04edd786dd76f95c221c6cb59954bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/srijan/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Srijan-23/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
