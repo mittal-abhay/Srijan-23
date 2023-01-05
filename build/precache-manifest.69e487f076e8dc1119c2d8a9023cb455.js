657bb186330998e8ac246544975242a05a7d1b7d
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-01-06 00:05:02 +0530

    n

---
+++ build/precache-manifest.69e487f076e8dc1119c2d8a9023cb455.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f31e496aaee79907c881252383f894",
+    "url": "/Srijan-23/index.html"
+  },
+  {
+    "revision": "72890ac557a83b66cd57",
+    "url": "/Srijan-23/static/css/2.7ea48c83.chunk.css"
+  },
+  {
+    "revision": "5abd8dfa49de77eab16c",
+    "url": "/Srijan-23/static/css/main.4219af1a.chunk.css"
+  },
+  {
+    "revision": "72890ac557a83b66cd57",
+    "url": "/Srijan-23/static/js/2.7b590526.chunk.js"
+  },
+  {
+    "revision": "5abd8dfa49de77eab16c",
+    "url": "/Srijan-23/static/js/main.2ab15878.chunk.js"
+  },
+  {
+    "revision": "1240834f292b5d62331f",
+    "url": "/Srijan-23/static/js/runtime-main.1707868c.js"
+  },
+  {
+    "revision": "5219973828099c873623d2279342ce36",
+    "url": "/Srijan-23/static/media/0.52199738.jpg"
+  },
+  {
+    "revision": "a3f85b7e15ef507fbf47180a537db0c2",
+    "url": "/Srijan-23/static/media/2.1.a3f85b7e.jpg"
+  },
+  {
+    "revision": "e65c0993f1c2eaa9c6937a845b0571c3",
+    "url": "/Srijan-23/static/media/28.1.e65c0993.jpg"
+  },
+  {
+    "revision": "c92af5f6bf6e39f5365555c4b65990aa",
+    "url": "/Srijan-23/static/media/3.c92af5f6.jpg"
+  },
+  {
+    "revision": "db557e2786e14ef13779e10d4baa150a",
+    "url": "/Srijan-23/static/media/38.db557e27.JPG"
+  },
+  {
+    "revision": "af046262201af6e114e1cb4a5ea9531f",
+    "url": "/Srijan-23/static/media/bg-black.af046262.webp"
+  },
+  {
+    "revision": "25e27511d4c8b94f0716b8438adac02a",
+    "url": "/Srijan-23/static/media/bg-wall.25e27511.png"
+  },
+  {
+    "revision": "554e1c077e1b7865b12debdfd334e0e7",
+    "url": "/Srijan-23/static/media/bg.554e1c07.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Srijan-23/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4a92410ffaa589bf6601160a5ff6c9cb",
+    "url": "/Srijan-23/static/media/golden_cursor.4a92410f.svg"
+  },
+  {
+    "revision": "1b8056d61c3d4109afcac14e0a27774a",
+    "url": "/Srijan-23/static/media/srijan_day_1.1b8056d6.jpg"
+  }
+]);
