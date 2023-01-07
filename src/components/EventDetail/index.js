cb51d8db0e989cef5a17d5db39c4eff356972c6d
Saumya Subhra Nandi <nandi.saumya2004@gmail.com>
2023-01-07 22:03:56 +0530

    margin updated

--- src/components/EventDetail/index.js
+++ src/components/EventDetail/index.js
@@ -34,7 +34,7 @@ const styles = (theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    marginLeft: "20vw",
+    marginLeft: "3vw",
     marginTop: "50px",
     [theme.breakpoints.down("sm")]: {
       marginLeft: "0px",
