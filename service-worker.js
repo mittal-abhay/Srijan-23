745ae8b8bc311bd5c76b6b12feb2181aca331749
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-01-08 14:56:19 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Srijan-23/precache-manifest.69e487f076e8dc1119c2d8a9023cb455.js"
+  "/Srijan-23/precache-manifest.fb3b459284b2c59ce53adc14f98ad8bf.js"
 );
 
 self.addEventListener('message', (event) => {
