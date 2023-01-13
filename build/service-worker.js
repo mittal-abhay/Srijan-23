f4afb0891dda1186a79fd096451d6b3212480243
kunal <kishujaiswal3@gmail.com>
2023-01-13 22:58:26 +0530

    build added

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Srijan-23/precache-manifest.ccb0a19b96b72da146af16ccdcde3282.js"
+  "/Srijan-23/precache-manifest.881aad41033b46d4dc4b0a71c9d9f0eb.js"
 );
 
 self.addEventListener('message', (event) => {
