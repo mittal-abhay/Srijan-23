92f9f0083f65e4d519da67b2471ed1adb3e1d0c3
ABHAY MITTAL <abhayhmittal@gmail.com>
2023-01-15 14:52:20 +0530

    build commited

---
+++ build/precache-manifest.7613f1b256a6d531481eb0f24a122e93.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1862bfba2ea1e46ad48298d425664e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4271f1302a7f68422b1",
+    "url": "/static/css/2.04566360.chunk.css"
+  },
+  {
+    "revision": "9d7b02b6874f15c3afee",
+    "url": "/static/css/main.56f64939.chunk.css"
+  },
+  {
+    "revision": "c4271f1302a7f68422b1",
+    "url": "/static/js/2.db46e1a7.chunk.js"
+  },
+  {
+    "revision": "9d7b02b6874f15c3afee",
+    "url": "/static/js/main.cab789a1.chunk.js"
+  },
+  {
+    "revision": "eacce1f7b4da1d20c9e4",
+    "url": "/static/js/runtime-main.2ae9fc14.js"
+  },
+  {
+    "revision": "ac4710bf1e7af33a1dac38f6fdf5228b",
+    "url": "/static/media/0.ac4710bf.webp"
+  },
+  {
+    "revision": "93e341e361d9a52e1cbc0e5e57a7382e",
+    "url": "/static/media/1.93e341e3.webp"
+  },
+  {
+    "revision": "32e8809d7546bf6fbff3d1831e4a40a1",
+    "url": "/static/media/2.1.32e8809d.webp"
+  },
+  {
+    "revision": "209437148c9e8050de1e7c0893cf4be4",
+    "url": "/static/media/3.20943714.webp"
+  },
+  {
+    "revision": "4a8f4b38f5113e043c0df65a2ad878eb",
+    "url": "/static/media/38.4a8f4b38.webp"
+  },
+  {
+    "revision": "1227f8a1160dad4732a741bf26c8c52e",
+    "url": "/static/media/Gallery3.1227f8a1.webp"
+  },
+  {
+    "revision": "9d59bba089e046acfc08c5631809a66f",
+    "url": "/static/media/Logo_Final.9d59bba0.png"
+  },
+  {
+    "revision": "a066a8fc8f13b3736abb54f97d119be1",
+    "url": "/static/media/Ongc.a066a8fc.png"
+  },
+  {
+    "revision": "1aff6fa34adef3570c46e7c30c93897a",
+    "url": "/static/media/RedBull.1aff6fa3.webp"
+  },
+  {
+    "revision": "a0944974f9f6ab744b25b88bd82c1c29",
+    "url": "/static/media/Reliance.a0944974.webp"
+  },
+  {
+    "revision": "0a45e7f5805e075d301e4baee0c32482",
+    "url": "/static/media/Srijan'23_Logo_White.0a45e7f5.png"
+  },
+  {
+    "revision": "55bb0eca8b4b63ad039eb01a3b83b8c6",
+    "url": "/static/media/VanHuesen.55bb0eca.png"
+  },
+  {
+    "revision": "51603eae6e228b6a0d741635d0c35668",
+    "url": "/static/media/backdrop.51603eae.webp"
+  },
+  {
+    "revision": "af046262201af6e114e1cb4a5ea9531f",
+    "url": "/static/media/bg-black.af046262.webp"
+  },
+  {
+    "revision": "25e27511d4c8b94f0716b8438adac02a",
+    "url": "/static/media/bg-wall.25e27511.png"
+  },
+  {
+    "revision": "beff8f7111e5b7bc1537acc4e716c3d3",
+    "url": "/static/media/cursor.beff8f71.svg"
+  },
+  {
+    "revision": "d27e53a914fe9b9413aa7277a634af88",
+    "url": "/static/media/darshan.d27e53a9.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "7256f2b75be7e2822e14c145664f830a",
+    "url": "/static/media/jubin.7256f2b7.png"
+  },
+  {
+    "revision": "ffa1d9af35cb875842dd0585142052a6",
+    "url": "/static/media/nb.ffa1d9af.png"
+  },
+  {
+    "revision": "67eb11f9eb492093b76097d409792a14",
+    "url": "/static/media/prof_gauri_shankar.67eb11f9.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c2c6c95047f889b648c21a16cd02380.js"
+  "/precache-manifest.7613f1b256a6d531481eb0f24a122e93.js"
 );
 
 self.addEventListener('message', (event) => {
